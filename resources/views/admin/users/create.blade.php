19caa1c90eed039597bfa4d78976ae350b6b14c7
Pato Fautsch <spfautschb@gmail.com>
2019-06-12 13:44:06 -0500

    User creation added, no photos

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Create new user</h1>
+
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group col-md-6">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            {!! Form::label('password', 'Password:') !!}
+            {!! Form::password('password', ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group col-md-6">
+            {!! Form::label('confirm_password', 'Confirm password:') !!}
+            {!! Form::password('confirm_password', ['class'=>'form-control']) !!}
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group col-md-3">
+            {!! Form::label('role_id', 'Role:' )  !!}
+            {!! Form::select('role_id', [''=>'Select role'] + $roles, null, ['class' => 'form-control' ]) !!}
+        </div>
+
+        <div class="form-group col-md-3">
+            {!! Form::label('is_active', 'Status:' )  !!}
+            {!! Form::select('is_active', [0 => 'Not Active', 1 => 'Active'],  0, ['class' => 'form-control' ]) !!}
+        </div>
+
+        <div class="form-group col-md-6">
+            {!! Form::label('photo_id', 'Upload photo:') !!}
+            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group col-md-12">
+            {!! Form::submit('Add User', ['class'=>'btn btn-primary pull-right']) !!}
+        </div>
+    </div>
+
+
+
+    {!! Form::close() !!}
+
+    <div class="form-row">
+        <div class="form-group col-md-12">
+            @include('includes.error')
+        </div>
+    </div>
+
+    @stop
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,67 @@
-<h1>It's working</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Users</h1>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <title>Bootstrap Example</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+    <div class="container">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>Created</th>
+              <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+        @if($users)
+            @foreach ($users as $user)
+
+                <tr>
+            <td>{{$user->id}}</td>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+                    <td>{{$user->role->name}}</td>
+                    @if ($user->is_active == 1)
+                        <td>Active</td>
+                    @else
+                        <td>Inactive</td>
+                    @endif
+                    @if ($user->created_at == null)
+                        <td>{{$user->created_at}}</td>
+                    @else
+                        <td>{{$user->created_at->diffForHumans()}}</td>
+                    @endif
+                    @if ($user->updated_at == null)
+                        <td>{{$user->updated_at}}</td>
+                    @else
+                        <td>{{$user->updated_at->diffForHumans()}}</td>
+                    @endif
+          </tr>
+
+            @endforeach
+        @endif
+        </tbody>
+      </table>
+    </div>
+
+    </body>
+    </html>
+
+    @endsection
