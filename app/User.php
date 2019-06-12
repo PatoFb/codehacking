af91f25fde83077eef90a2651a806bff29f373c6
Pato Fautsch <spfautschb@gmail.com>
2019-06-12 18:08:43 -0500

    users with photos feature working

--- app/User.php
+++ app/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
     public function role(){
         return $this->belongsTo('App\Role');
     }
+
+    public function photo(){
+        return $this->belongsTo('App\Photo');
+    }
+
 }
